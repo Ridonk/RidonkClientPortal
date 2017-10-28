2dc8d58c1fa0a5e9f4b376d5e3662d97c3438f99
Ridonk <georgeddunbar@gmail.com>
2017-10-28 08:16:07 -0400

    Staging new profile system

---
+++ Project/public/default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georgeddunbar
+ * Date: 10/28/17
+ * Time: 8:13 AM
+ */
+
+?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title><?php echo 'UsernameHere'; ?></title>
+    </head>
+    <body>
+    <h1>UsernameHere Profile</h1>
+    </body>
+    </html>
+<?php
